b94e2fb3161580444c741b0df82e37efd7ab7ae7
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-06-25 20:21:04 +0000

    its okay now

---
+++ wordaday/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.wordaday',
+  appName: 'wordaday',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ wordaday/src/components/ExploreContainer.tsx
@@ -0,0 +1,18 @@
+import './ExploreContainer.css';
+import Test1 from './Test1';
+
+interface ContainerProps {
+  name: string;
+}
+
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  return (
+    <div className="container">
+      <strong>{name}</strong>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <Test1 />
+    </div>
+  );
+};
+
+export default ExploreContainer;
